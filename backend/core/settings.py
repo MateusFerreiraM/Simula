0530090870c406cab0d9200bde29f54807f72b0d
MateusFerreiraM <mateusferreira@id.uff.br>
2025-08-18 20:41:32 -0300

    Cloudinary para imagens

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'djoser',
+    'cloudinary_storage',
+    'cloudinary',
 
     # Nossos apps
     'simula',
@@ -143,3 +145,11 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
 }
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
