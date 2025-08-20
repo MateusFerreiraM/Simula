1d5ea5fe272d33f6edbcc2a8e9bc0afa6c5efb81
MateusFerreiraM <mateusferreira@id.uff.br>
2025-08-19 23:25:46 -0300

    feat: Lança a versão 1.0 do Simula

--- backend/build.sh
+++ backend/build.sh
@@ -4,6 +4,7 @@ set -o errexit
 
 pip install -r requirements.txt
 
+# Bloco de Limpeza Condicional
 if [ "$RUN_FLUSH" = "True" ]; then
   echo "!!! VARIÁVEL RUN_FLUSH DETETADA - LIMPANDO O BANCO DE DADOS !!!"
   python manage.py flush --no-input
@@ -12,4 +13,10 @@ fi
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Carrega os dois fixtures, em ordem
+echo "Carregando fixture de questões..."
 python manage.py loaddata questoes_finais.json
+
+echo "Carregando fixture de usuário admin..."
+python manage.py loaddata admin_user.json
