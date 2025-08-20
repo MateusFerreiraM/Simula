1106d1a5ed565ebcfedba9edde6ca7c5f82d5c05
MateusFerreiraM <mateusferreira@id.uff.br>
2025-08-19 21:20:54 -0300

    Adiciona script de limpeza e fixture final com admin

--- backend/build.sh
+++ backend/build.sh
@@ -4,6 +4,13 @@ set -o errexit
 
 pip install -r requirements.txt
 
+# Bloco de Limpeza Condicional
+if [ "$RUN_FLUSH" = "True" ]; then
+  echo "!!! VARIÁVEL RUN_FLUSH DETETADA - LIMPANDO O BANCO DE DADOS !!!"
+  python manage.py flush --no-input
+  echo "!!! BANCO DE DADOS LIMPO COM SUCESSO !!!"
+fi
+
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py loaddata questoes_finais.json
+python manage.py loaddata dados_finais_com_admin.json
